7386e5a0635e50c31133a21c01b8bb510b28da98
Leo Balter <leonardo.balter@gmail.com>
2011-09-12 14:14:59 -0300

    adding minified js (w/ uglifyJS) and creating a new jQueryfied way to call the menu, check index.html for examples

--- js/menu.js
+++ js/menu.js
@@ -1,53 +1,52 @@
-jQuery(function($) {
-    var HorizontalMenu = (function($, window, undefined) {
-        return function(context) {
-            var currentActive, $context, $menuBar;
+var HorizontalMenu = (function($, window, undefined) {
+    return function(context) {
+        var currentActive, $context, $menuBar;
 
-            var init = function() {
-                $context = $(context);
-                $menuBar = $context.find('.menuBar');
-                currentActive = $context.find('.currentActive');
-                if (!$context.length || !$menuBar.length || !currentActive.length) {
-                    if (console && console.log) {
-                        console.log('something is missing in your horiz. menu');
-                    }
-                    return;
+        var init = function() {
+            $context = $(context);
+            $menuBar = $context.find('.menuBar');
+            currentActive = $context.find('.currentActive');
+            if (!$context.length || !$menuBar.length || !currentActive.length) {
+                if (console && console.log) {
+                    console.log('something is missing in your horiz. menu');
                 }
-                move(currentActive);
-                setEvents();
-            };
-
-            var move = function(elem) {
-                var left, width;
-                left = elem.position().left + 4;
-                width = elem.width();
-                $menuBar.stop(true).delay(200).animate({
-                    left: left,
-                    width: width
-                }, 600);
-            };
-
-            var setEvents = function() {
-                $context.find('li').hover(function(ev) {
-                    if (ev.type === 'mouseenter') {
-                        var $this = $(this);
-                        move($this);
-                    } else if (ev.type === 'mouseleave') {
-                        move(currentActive);
-                    }
-                });
-            };
-
-            return {
-                init : init
-            };
+                return;
+            }
+            move(currentActive);
+            setEvents();
         };
-    }(jQuery, window));
 
-    var myMenu = new HorizontalMenu('#myMenu');
-    myMenu.init();
+        var move = function(elem) {
+            var left, width;
+            left = elem.position().left + 4;
+            width = elem.width();
+            $menuBar.stop(true).delay(200).animate({
+                left: left,
+                width: width
+            }, 600);
+        };
 
-    var myMenu2 = new HorizontalMenu('#myMenu2');
-    myMenu2.init();
-    
-});
+        var setEvents = function() {
+            $context.find('li').hover(function(ev) {
+                if (ev.type === 'mouseenter') {
+                    var $this = $(this);
+                    move($this);
+                } else if (ev.type === 'mouseleave') {
+                    move(currentActive);
+                }
+            });
+        };
+
+        return {
+            init : init
+        };
+    };
+}(jQuery, window));
+
+// you want it as a jquery plugin? all right
+jQuery.fn.horizontalMenu = function () {
+    return jQuery(this).each(function() {
+        var newMenu = new HorizontalMenu(this);
+        newMenu.init();
+    });
+};
